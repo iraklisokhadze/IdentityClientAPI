33d2468acc0c28f383d894907e4c661035f02a15
Irakli Sokhadze <Irakli.Sokhadze@outlook.com>
2021-02-21 19:53:22 +0400

    minor fix

--- IdentityClient.Infrastructure.RelationDatabase/Mapper.cs
+++ IdentityClient.Infrastructure.RelationDatabase/Mapper.cs
@@ -7,7 +7,9 @@ namespace IdentityClient.Infrastructure.RelationDatabase
         public Mapper()
         {
             CreateMap<Core.Models.User, User>()
-                .ForMember(u=>u.SecurityStamp , x=> x.Ignore())
+                .ForMember(u => u.SecurityStamp, x => x.Ignore())
+                .ForMember(u => u.EmailConfirmed, x => x.MapFrom(a => true))
+                .ForMember(u => u.PhoneNumberConfirmed, x => x.MapFrom(a => true))
                 .ReverseMap();
             CreateMap<Core.Models.Address, Address>().ReverseMap();
         }
